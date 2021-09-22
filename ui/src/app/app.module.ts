bbd71090265bd29c0c924297d8d1387e453533ca
Yogi <wei.akai@gmail.com>
2021-09-21 22:20:04 -0400

    Basic Real Time

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,18 +4,24 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './components/shared/shared.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule, DatePipe } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
     imports: [
-    BrowserModule,
     AppRoutingModule,
-    SharedModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    CommonModule,
+    FlexLayoutModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    DatePipe
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ui/src/app/components/home/home.component.ts
+++ ui/src/app/components/home/home.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Subscription, timer } from 'rxjs';
+import { share, map } from 'rxjs/operators';
+import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  rxTime: String;
+  subscription: Subscription;
+  
+  constructor(private datePipe: DatePipe) { }
+  
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.subscription = timer(0, 1000)
+      .pipe(
+        map(() => new Date()),
+        share()
+      )
+      .subscribe(time => {
+        this.rxTime = this.datePipe.transform(time, 'hh:mm:ss a');
+      });
   }
 
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
 }
